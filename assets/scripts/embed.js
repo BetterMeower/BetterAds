ce92e7a547ad9bd62dcdc6f5af0df7f1fbcce49b
Daniel Barton <mineminecraftpe687@gmail.com>
2022-03-12 14:07:40 +1100

    Create embed.js

---
+++ assets/scripts/embed.js
@@ -0,0 +1,3 @@
+document.getElementById('daniel4ScratchAdvert').src = "https://daniel4-scratch.is-a.dev/adverts/generate"
+document.getElementById('daniel4ScratchAdvert').style = "border:none; width:728px; height:90px; overflow:hidden;"
+document.getElementById('daniel4ScratchAdvert').scrolling  = "no"

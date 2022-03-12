477e021aa8151f69b0c3b389447893e1c73f1c6c
Daniel Barton <mineminecraftpe687@gmail.com>
2022-03-12 15:19:46 +1100

    Update embed.js

--- assets/scripts/embed.js
+++ assets/scripts/embed.js
@@ -1,3 +1,3 @@
 document.getElementById('daniel4ScratchAdvert').src = "https://daniel4-scratch.is-a.dev/adverts/generate"
-document.getElementById('daniel4ScratchAdvert').style = "border:none; width:728px auto; max-width:100%; height:90px auto; max-height:100%; overflow:hidden;"
+document.getElementById('daniel4ScratchAdvert').style = "border:none; width:728px; height:90px; overflow:hidden;"
 document.getElementById('daniel4ScratchAdvert').scrolling  = "no"

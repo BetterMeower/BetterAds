b8f2a1f9735ee43e1c2dafb9093fd048d2f4631e
silvxrcat <silvxrcat@gmail.com>
2023-01-07 18:35:44 -0600

    generate with class + improve code

--- assets/scripts/embed.js
+++ assets/scripts/embed.js
@@ -1,3 +1,13 @@
-document.getElementById('BetterAd').src = "https://adservice.bettermeower.app/generate"
-document.getElementById('BetterAd').style = "border:none; width:728px; height:90px; overflow:hidden;"
-document.getElementById('BetterAd').scrolling  = "no"
+let ads = document.getElementsByClassName('BetterAd')
+let warn = 0
+
+ads.forEach(e => {
+    if (e.tagName === "iframe") {
+        e.src = "https://adservice.bettermeower.app/generate"
+        e.style = "border:none; width:728px; height:90px; overflow:hidden;"
+        e.scrolling = "no"
+    } else {
+        warn++
+        console.warn(`${warn} elements were found with the BetterAds class, but not an iframe.`);
+    }
+})

146b72320b0cdd9c9c055f2c96f5446f1b7d9e3d
silvxrcat <silvxrcat@gmail.com>
2023-01-07 18:41:07 -0600

    custom.js is unneeded

--- assets/scripts/custom.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('BetterAd').src = "https://adservice.bettermeower.app/generate"
-document.getElementById('BetterAd').style = "width:728px; height:90px; overflow:hidden;"
-document.getElementById('BetterAd').scrolling  = "no"
